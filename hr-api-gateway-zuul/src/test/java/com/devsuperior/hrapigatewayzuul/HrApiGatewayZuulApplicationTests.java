6db7be3eebaed094037b52a00cec1c664b2ea397
Gilberto Feitosa Gavioli <gilberto.gavioli@gmail.com>
2021-06-25 09:59:08 -0300

    Commit final dos exercicios

--- hr-api-gateway-zuul/src/test/java/com/devsuperior/hrapigatewayzuul/HrApiGatewayZuulApplicationTests.java
+++ hr-api-gateway-zuul/src/test/java/com/devsuperior/hrapigatewayzuul/HrApiGatewayZuulApplicationTests.java
@@ -3,7 +3,7 @@ package com.devsuperior.hrapigatewayzuul;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+ 
 class HrApiGatewayZuulApplicationTests {
 
 	@Test
